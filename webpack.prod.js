6d7b7954de9412081adb77c2ee1e195d4cebe2ad
Chris Palka <cpalka87gmail.com>
2020-08-08 00:04:13 -0600

    Separate weback config.. prod vs dev   -

---
+++ webpack.prod.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common');
+
+const DIST_DIR = path.join(__dirname, '/react-client/dist');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: 'bundle[contentHash].js',
+    path: DIST_DIR,
+  },
+});
