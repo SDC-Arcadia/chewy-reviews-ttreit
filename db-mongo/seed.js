f3c3fb02eb04b40427b63e8d8fb12f7767e9402e
Chris Palka <cpalka87gmail.com>
2020-08-08 22:09:21 -0600

    Add api calls to server   - Retrieve all reviews or review by product_id   - /reviews vs /review (product_id)   - Import db in to server to query directly

--- db-mongo/seed.js
+++ db-mongo/seed.js
@@ -1,53 +1,54 @@
+/* eslint-disable no-nested-ternary */
+const fetch = require('node-fetch');
 const db = require('./index.js');
 const Reviews = require('./Review.js');
-const fetch = require('node-fetch');
 
-var count = 0;
-const names = "ediblesurf\nscarypepperoni\ncorkbaggage\nauctioneerhatching\nsnoutfea\nimmersionmanrope\nmuralmilly\nseventhneodymium\nchunksalt\nreformatmagpie\nlabradortwister\nregimepeese\nstaratlas\nflashsixth\ncancerkite\nraymondcurtains\ncreamlikecherub\nparalysesprogress\nabaftproposal\narticulateamos\nsaskatoonfrench\nimpressdarkened\nglottlethrower\ndindowssoulful\nbottomherald\nbrunnichandes\nmeddarsleather\nneodymiumspirit\nnuptsesnazzy\nafraidunbridle\nmoonlighttoilet\ncomplexorfling\nhuskinesscash\nevokeelbrus\nfadedadvance\ndullaffix\nverbclattering\njellydeplete\ncriteriacatnap\nhominybust\ninfiniteorrin\nnetworkparking\njungeecynon\ndeclarestorm\npneumoniaski\narmoryaccentor\naudacityjoints\njoggercycle\nnicotineswunchy\nmanagerpops\njoyfulneptunium\nwoundswilderness\nmyviewpopulation\npetchtasha\nstrodeils\nconfusionendpoint\nbunkbedfog\npetrifiedoklahoma\naideamidships\nbroadsnugly\nobjectfunction\nsiestaequate\nquizzicalclydach\ncathousepup\nrumblelizard\nphonydivide\nricesymphony\ndreefhootenany\ndailypowdered\nkivadryer\nsandyconcede\ntaodailgermanium\nfrompoking\nsummergrape\ncharlesculprit\nlateplutting\njetfoudry\ngenespittal\nglassesballs\ndemplewheatear\nairsnatch\nsingerharm\noilymones\nbooesreborn\nrewordfunnels\nleanregistered\nfiddleextreme\nimitategravity\nrubyaffidavit\nfuneralyudding\ngrainmodeling\nkindergartenleaves\ndampedaudition\nadaptivesixfold\nbiopsygore\nfilesdosty\ncarefreelikeable\ndareslaxative\nhearingpacify\njudysleuth\nscrambleralph\nwimpfrisbee\nmadlyalexandria\nhearingcornball\nbrakesmall\nbreedercitrusy\nlogsprint\ngoldmedalraise\nhelpfulyikes\nramblingangie\nshakablehamburger\nticketsfist\ntowerwegs\nexactingharmony\npostsuncouth\nbootlesssimon\nviewingcamera\nwirelessbased\nfoecadair\narmaghfarmer\ndramatictrifocals\ntyingmid\ncrippsvimeo\ncrumblyscotland\nvaselinelila\nnuggedlegwarmer\nalkalineduress\ndicedoil\nbarblingradon\nbilliesblank\nflapjackwarbler\ngrowldirective\ndepletewise\ntatchjawed\ntartloxahatchee\nrailwaysadly\ntelephonestumptown\npagingnatural\nassiniboinepoppy\nrackunsuitable\nbitfeverish\nsmallweedalso\nmournernest\nheritagefollowed\ncoatgift\ncompetitoralibi\nmogglingquantity\nvermontswaddling\nectodermmalestate\nshooterforever\nshackalgae\nperseusjoin\nfruitwalker\nconsisttelrad\npoetsnorkel\nmonthlyopulent\npowerhart\nmowingjellies\nsoakanchor\nintelunderstanding\npickclassroom\npicayunenutcase\nunsuretriathlon\nfoggystickers\nflotillanicoise\ngreaterstaking\nfaildial\ntoolhush\ndexterousdecay\ncunseymumbling\nfionaquaint\nkelvinchomp\nmightyunison\nchoardvest\ntackingskylight\ndevelopqueer\npolythenekansas\ntannedcountry\nclawedfreely\nfledfix\ncabotatrophy\namigoexcluded\nvestashamed\nconfesssnictor\nbugtorque\napigiftshop\ncreepyuninsured\nvallislate\nbathroomdiving\nplinkoval\nhusclejukebox\nnagdeadbeat\nsplendidspiffy\nbipplingavenge\nfrizzylouie\ntwigpumpkin\nobedientmontana\nkittbaloo\npariscenters\nparcelpotter".split('\n');
+const names = 'ediblesurf\nscarypepperoni\ncorkbaggage\nauctioneerhatching\nsnoutfea\nimmersionmanrope\nmuralmilly\nseventhneodymium\nchunksalt\nreformatmagpie\nlabradortwister\nregimepeese\nstaratlas\nflashsixth\ncancerkite\nraymondcurtains\ncreamlikecherub\nparalysesprogress\nabaftproposal\narticulateamos\nsaskatoonfrench\nimpressdarkened\nglottlethrower\ndindowssoulful\nbottomherald\nbrunnichandes\nmeddarsleather\nneodymiumspirit\nnuptsesnazzy\nafraidunbridle\nmoonlighttoilet\ncomplexorfling\nhuskinesscash\nevokeelbrus\nfadedadvance\ndullaffix\nverbclattering\njellydeplete\ncriteriacatnap\nhominybust\ninfiniteorrin\nnetworkparking\njungeecynon\ndeclarestorm\npneumoniaski\narmoryaccentor\naudacityjoints\njoggercycle\nnicotineswunchy\nmanagerpops\njoyfulneptunium\nwoundswilderness\nmyviewpopulation\npetchtasha\nstrodeils\nconfusionendpoint\nbunkbedfog\npetrifiedoklahoma\naideamidships\nbroadsnugly\nobjectfunction\nsiestaequate\nquizzicalclydach\ncathousepup\nrumblelizard\nphonydivide\nricesymphony\ndreefhootenany\ndailypowdered\nkivadryer\nsandyconcede\ntaodailgermanium\nfrompoking\nsummergrape\ncharlesculprit\nlateplutting\njetfoudry\ngenespittal\nglassesballs\ndemplewheatear\nairsnatch\nsingerharm\noilymones\nbooesreborn\nrewordfunnels\nleanregistered\nfiddleextreme\nimitategravity\nrubyaffidavit\nfuneralyudding\ngrainmodeling\nkindergartenleaves\ndampedaudition\nadaptivesixfold\nbiopsygore\nfilesdosty\ncarefreelikeable\ndareslaxative\nhearingpacify\njudysleuth\nscrambleralph\nwimpfrisbee\nmadlyalexandria\nhearingcornball\nbrakesmall\nbreedercitrusy\nlogsprint\ngoldmedalraise\nhelpfulyikes\nramblingangie\nshakablehamburger\nticketsfist\ntowerwegs\nexactingharmony\npostsuncouth\nbootlesssimon\nviewingcamera\nwirelessbased\nfoecadair\narmaghfarmer\ndramatictrifocals\ntyingmid\ncrippsvimeo\ncrumblyscotland\nvaselinelila\nnuggedlegwarmer\nalkalineduress\ndicedoil\nbarblingradon\nbilliesblank\nflapjackwarbler\ngrowldirective\ndepletewise\ntatchjawed\ntartloxahatchee\nrailwaysadly\ntelephonestumptown\npagingnatural\nassiniboinepoppy\nrackunsuitable\nbitfeverish\nsmallweedalso\nmournernest\nheritagefollowed\ncoatgift\ncompetitoralibi\nmogglingquantity\nvermontswaddling\nectodermmalestate\nshooterforever\nshackalgae\nperseusjoin\nfruitwalker\nconsisttelrad\npoetsnorkel\nmonthlyopulent\npowerhart\nmowingjellies\nsoakanchor\nintelunderstanding\npickclassroom\npicayunenutcase\nunsuretriathlon\nfoggystickers\nflotillanicoise\ngreaterstaking\nfaildial\ntoolhush\ndexterousdecay\ncunseymumbling\nfionaquaint\nkelvinchomp\nmightyunison\nchoardvest\ntackingskylight\ndevelopqueer\npolythenekansas\ntannedcountry\nclawedfreely\nfledfix\ncabotatrophy\namigoexcluded\nvestashamed\nconfesssnictor\nbugtorque\napigiftshop\ncreepyuninsured\nvallislate\nbathroomdiving\nplinkoval\nhusclejukebox\nnagdeadbeat\nsplendidspiffy\nbipplingavenge\nfrizzylouie\ntwigpumpkin\nobedientmontana\nkittbaloo\npariscenters\nparcelpotter'.split('\n');
 
-const insertSampleData = function (type = 'hipster-centric', paras = 5) {
-  const randomNum = (max) => {
-    return Math.floor(Math.random() * Math.floor(max))
-  }
+let count = 0;
+const insertSampleData = (type = 'hipster-centric', paras = 5) => {
+  const randomNum = (min, max) => Math.floor(Math.random() * (max - min) + min);
   const sampleReviews = [];
 
-  const url = `https://hipsum.co/api/?type=${type}&paras=${paras}`
+  const url = `https://hipsum.co/api/?type=${type}&paras=${paras}`;
   fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    data.forEach((x) => {
-        let productId = count < 10 ? `P00${count}` : count > 9 && count < 100 ? `P0${count}` : `P${count}`
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((x) => {
+        const productId = count < 10 ? `P00${count}` : count > 9 && count < 100 ? `P0${count}` : `P${count}`;
         const productReview = {
-          productId: productId,
-          averageRating: randomNum(100),
-          reviews: []
+          product_id: productId,
+          average_rating: randomNum(1, 100),
+          reviews: [],
+        };
+
+        for (let i = 0; i < randomNum(1, 11); i += 1) {
+          const review = {
+            author: names[randomNum(1, 200)],
+            create_date: Date.now(),
+            body: x,
+            likes: randomNum(1, 50),
+            stars: randomNum(1, 6),
+            recommended: randomNum(1, 11) % 2 !== 0,
+          };
+          productReview.reviews.push(review);
         }
 
-        for (var i = 0; i < randomNum(11); i++) {
-          const review = {
-            author: names[randomNum(200)],
-            body: x,
-            likes: randomNum(50),
-            stars: randomNum(6),
-            recommended: count % 2 === 0 ? false : true
-          }
-          productReview.reviews.push(review)
-        }
-        count++;
-        sampleReviews.push(productReview)
-      })
+        count += 1;
+        sampleReviews.push(productReview);
+      });
       return sampleReviews;
     })
-    .then(data => {
+    .then((data) => {
       Reviews.create(data)
         .then(() => db.disconnect());
-    })
+    });
 };
 
 const loopSampleData = (num, type, paras) => {
-  for (var i = 0; i < num; i++) {
+  for (let i = 0; i < num; i += 1) {
     insertSampleData(type, paras);
   }
-}
+};
 
 loopSampleData(10, 'hipster-centric', 50);
