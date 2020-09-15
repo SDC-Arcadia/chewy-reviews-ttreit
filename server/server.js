1c272e5bfd1005c689924009fb274bbe6e69494d
Chris Palka <cpalka87gmail.com>
2020-09-14 18:07:30 -0600

    Create endpoint to filter stars dynamically

--- server/server.js
+++ server/server.js
@@ -64,6 +64,21 @@ app.get('/reviewSummary/:productId', (req, res) => {
   });
 });
 
+app.get('/filterStars/:productId/:starRating', (req, res) => {
+  const { starRating } = req.params;
+  const { productId } = req.params;
+  Reviews.findOne({ product_id: productId.toUpperCase() }, (err, result) => {
+    if (!result) {
+      console.log('Error querying database! ', err);
+      res.sendStatus(404);
+    } else {
+      const match = result.reviews.filter((reviews) => reviews.stars === (Number(starRating)));
+      console.log('Filtered Result: ', match);
+      res.sendStatus(200);
+    }
+  });
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/../react-client/dist', 'index.html'));
 });
