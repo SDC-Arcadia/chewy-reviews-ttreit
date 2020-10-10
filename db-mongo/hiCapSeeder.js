27a4908a5b4103e6b671d30fceadaaa0140e327b
ttreit <14933277+ttreit@users.noreply.github.com>
2020-10-09 19:36:02 -0700

    seeder WIP

--- db-mongo/hiCapSeeder.js
+++ db-mongo/hiCapSeeder.js
@@ -1,4 +1,27 @@
 const faker = require('faker');
+const fs = require('fs');
 
-console.log('likes', faker.random.number(200));
+function generateFakeReview () {
+  let fakeReviews = [];
+  for (i = 0; i < 10; i++) {
+    let fakeLikes = faker.random.number(105);
+    let fakeStars = faker.random.number(5);
+    let fakeTitle = faker.lorem.sentence();
+    let fakeAuthor = faker.name.findName();
+    let fakeBody = faker.lorem.paragraph();
+    let fakeRecommended = faker.random.boolean();
+    fakeReviews.push(
+      {
+        "likes": fakeLikes,
+        "stars": fakeStars,
+        "title": fakeTitle,
+        "author": fakeAuthor,
+        "body": fakeBody,
+        "recommended": fakeRecommended
+      }
+    )
+  }
+}
+
+
 
