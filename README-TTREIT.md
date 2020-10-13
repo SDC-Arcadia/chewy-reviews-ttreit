5bf70c01229e093d350e01c999411b98f781bca0
ttreit <14933277+ttreit@users.noreply.github.com>
2020-10-12 18:38:37 -0700

    WIP

---
+++ README-TTREIT.md
@@ -0,0 +1,18 @@
+#Documentation for System Design
+
+##Reviews Form UI
+- Do you recommend (checkbox)
+- Star rating (1-5)
+- Nickname
+- Headline for Review
+- Review Text
+- Add photo
+
+##Reviews - API Reference
+GET /reviewData/:productId
+GET /reviewSummary/:productId
+GET /filterReviews/:productId/:starRating
+
+
+##Node Version
+14.13.1
--- db-mongo/hiCapSeeder.js
+++ db-mongo/hiCapSeeder.js
@@ -1,27 +1,28 @@
 const faker = require('faker');
-const fs = require('fs');
+const { createReadStream, createWriteStream } = require('fs')
 
-function generateFakeReview () {
-  let fakeReviews = [];
-  for (i = 0; i < 10; i++) {
+function generateFakeReviews () {
+  //  create source
+  for (i = 0; i < 5; i++) {
     let fakeLikes = faker.random.number(105);
     let fakeStars = faker.random.number(5);
     let fakeTitle = faker.lorem.sentence();
     let fakeAuthor = faker.name.findName();
     let fakeBody = faker.lorem.paragraph();
     let fakeRecommended = faker.random.boolean();
-    fakeReviews.push(
-      {
-        "likes": fakeLikes,
-        "stars": fakeStars,
-        "title": fakeTitle,
-        "author": fakeAuthor,
-        "body": fakeBody,
-        "recommended": fakeRecommended
-      }
-    )
+
+    let review = `${fakeLikes}, ${fakeStars}, ${fakeTitle}, ${fakeAuthor}, ${fakeBody}, ${fakeRecommended}`;
+    console.log(i);
+    console.log(review);
   }
 }
+generateFakeReviews();
 
+//TODO review stream pipeline and how streaming and pipeline work
 
+// const testLine = 'Likes, Stars, Title, Author, Body, Recommended\n'
+// const testLine2 = '12, 3, My Title, Tammy, Body of review, true\n'
+// let testWriteStream = fs.createWriteStream(__dirname + '/testCsvDoc.csv');
 
+// testWriteStream.write(testLine);
+// testWriteStream.write(testLine2);
