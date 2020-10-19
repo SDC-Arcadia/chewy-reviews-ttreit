217583b1cb4377d572a27b07fafe855c77ff6b2b
ttreit <14933277+ttreit@users.noreply.github.com>
2020-10-19 11:52:47 -0700

    Update seeder to create millions of records

--- db-mongo/hiCapSeeder2.js
+++ db-mongo/hiCapSeeder2.js
@@ -1,33 +1,39 @@
 const faker = require('faker');
-const { createReadStream, createWriteStream } = require('fs')
+const { createReadStream, createWriteStream } = require('graceful-fs')
 
-function generateFakeReviews(totalBatches, batchSize, fileName) {
-  let header = createWriteStream(fileName);
+function generateFakeReviews (itemQuantity, filename) {
+  let header = createWriteStream(filename);
   header.write('productid, likes, stars, title, author, body, recommended\n');
-  let reviewCounter = 0;
-  for (let k = totalBatches; k > 0; k--) {
-    for (let i = 0; i < batchSize; i++) {
-      let randomNumberOfReviews = Math.floor(Math.random() * 5) + 1;
-      //let randomNumberOfReviews = 1;
-      for (let j = randomNumberOfReviews; j > 0; j--) {
-        let productId = i + 1;
-        let fakeLikes = faker.random.number(105);
-        let fakeStars = faker.random.number(5);
-        let fakeTitle = faker.lorem.sentence();
-        let fakeAuthor = faker.name.findName();
-        let fakeBody = faker.lorem.paragraph();
-        let fakeRecommended = faker.random.boolean();
+  let counter = 0;
 
-        let review = `${productId}, ${fakeLikes}, ${fakeStars}, ${fakeTitle}, ${fakeAuthor}, ${fakeBody}, ${fakeRecommended}\n`;
-        reviewCounter++;
-        let reviewsWriteStream = createWriteStream(fileName, { flags: 'a' });
-        reviewsWriteStream.write(review);
-      }
-    }
-  }
-  console.log(`Total Reviews: ${reviewCounter}`)
+  const reviewsWriteStream = createWriteStream(filename, {flags: 'a'});
+
+
+  (async () => {
+      for (i = 0; i < itemQuantity; i++) {
+        let randomNumberOfReviews = Math.floor(Math.random() * 5) + 1;
+
+        for (j = randomNumberOfReviews; j > 0; j--) {
+          let productId = i + 1;
+          let fakeLikes = faker.random.number(105);
+          let fakeStars = faker.random.number(5);
+          let fakeTitle = faker.lorem.sentence();
+          let fakeAuthor = faker.name.findName();
+          let fakeBody = faker.lorem.paragraph();
+          let fakeRecommended = faker.random.boolean();
+
+          let review = `${productId}, ${fakeLikes}, ${fakeStars}, ${fakeTitle}, ${fakeAuthor}, ${fakeBody}, ${fakeRecommended}\n`;
+          counter ++;
+          if (!reviewsWriteStream.write(review)) {
+            await new Promise(resolve => reviewsWriteStream.once('drain', resolve));
+          }
+          }
+        } //end loop
+        console.log (`Total Reviews: ${counter}`)
+    })(); // ()???
+
+
 }
 
-
-generateFakeReviews(10, 1000, 'reviews.csv');
+generateFakeReviews(10000000, 'hiCap2.csv');
 
